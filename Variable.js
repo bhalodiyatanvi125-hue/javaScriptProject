72e6ccb26ab105de1a313f6dd8bf88b40d666c35
Tanvi Bhalodiya <bhalodiyatanvi125@gmail.com>
2026-01-28 21:48:30 +0530

    [BT][variable.js file added]

---
+++ Variable.js
@@ -0,0 +1,26 @@
+// Variable
+//Var
+//const
+//let
+
+// var
+
+// var a=10;
+// console.log("Variable :-",a);
+
+
+//let
+
+// let a=10;
+// if(true)
+// {
+//     a=20;
+// }
+// console.log("Let",a);
+
+
+
+// const
+
+// const pi=3.14;
+// console.log("Const:-",pi);
---
+++ datatype.js
@@ -0,0 +1,59 @@
+//Datatype
+//primitive datatype
+//   Number
+//   BigInt
+//   String
+//   Boolean
+//   null
+//   Symbol
+// non primitive datatype
+// Array
+// Object
+// Function
+
+// number
+
+
+// let num =10;
+// let num1=20;
+
+// console.log("number :-",num ,num1);
+
+
+
+// String
+
+// let num="tanvi";
+// let num1="Dhruvi";
+
+// console.log("String :-",num,num1);
+
+//BigInt
+
+// let BigInt=12345678998765n;
+// console.log("BigInt:-",BigInt);
+
+
+//Boolean
+
+// let isTrue=true;
+// let isFalse=false;
+
+// console.log("Boolean :-",isTrue);
+
+
+
+//undefined
+
+// let a;
+// console.log("undefined",a);
+
+
+//null
+
+// let a=null;
+// console.log("Null",a);
+
+
+// Non-primitive Datatype
+
